e7662c9fb39b7cce171e11620fab766240ca85e0
陈保菁 <2969271422@qq.com>
2021-10-15 10:33:02 +0800

    First Commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){    /* 获取到浏览器顶端距离 */
+    var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+    /* console.log(scrollT); */
+    var backTop = $("#world").offset().top - $(window).height()/2;
+    console.log(backTop);
+
+    if(scrollT > backTop){
+        $(".a1").addClass("animated bounceInRight show").removeClass("fade");
+        $(".a2").addClass("animated bounceInDown show").removeClass("fade");
+        $(".a3").addClass("animated bounceInUp show").removeClass("fade");
+        $(".a4").addClass("animated bounceInLeft show").removeClass("fade");
+    }
+});
